3a67a8dcc627440011ce7b54d846db52d2b3d839
laavang <laavang@gmail.com>
2020-06-03 20:05:34 -0500

    Added AppBar and some react components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/core/MenuIcon';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className="" color="inherit" aria-label="menu">
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h6" className="">
+            News
+    </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+
     </div>
   );
 }
